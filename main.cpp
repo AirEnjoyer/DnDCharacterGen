cdc33b6e5c1f16b89eda61ed3cca23af098a2ce3
AirEnjoyer <nicholasmilligan2019@outlook.com>
2025-10-03 02:23:34 -0700

    Updated, not working but I'm working on it

--- main.cpp
+++ main.cpp
@@ -49,14 +49,12 @@ void getHitDice(int *hitDiceType, int *characterClass) {
 }
 
 void getMaxHP(int *maxHP, int *characterLevel, int *hitDiceType) {
-  srand((unsigned int)time(NULL));
   for (int i = 0; i < *characterLevel - 1; ++i) {
     *maxHP += (rand() % *hitDiceType - 1) + 1;
   }
 }
 
 void getStat(int *statSum, int *rolls, int amountOfRolls) {
-  srand((unsigned int)time(NULL));
   for (int j = 0; j < 6; j++) {
     *statSum = 0;
     for (int i = 0; i < amountOfRolls; i++) {
@@ -69,12 +67,22 @@ void getStat(int *statSum, int *rolls, int amountOfRolls) {
 }
 
 int main() {
-
+  srand(static_cast<unsigned int>(time(NULL)));
   system("clear");
+
+  std::map<std::string, int *> statMap = {
+      {"Strength", &Strength},         {"Dexterity", &Dexterity},
+      {"Constitution", &Constitution}, {"Wisdom", &Wisdom},
+      {"Intelligence", &Intelligence}, {"Charisma", &Charisma}};
 
   const int amountOfRolls = 4;
   int rolls[amountOfRolls];
   int statSum, characterLevel, healthSum, characterClass, hitDiceType;
+  int Strength, Dexterity, Constitution, Wisdom, Intelligence, Charisma;
+  std::vector<std::string> statNames = {"Strength",     "Dexterity",
+                                        "Constitution", "Wisdom",
+                                        "Intelligence", "Charisma"};
+
   std::string classes[13]{"Barbarian", "Bard",    "Blood Hunter", "Cleric",
                           "Druid",     "Fighter", "Monk",         "Paladin",
                           "Ranger",    "Rogue",   "Sorcerer",     "Warlock",
@@ -107,9 +115,45 @@ int main() {
   system("clear");
   std::cout << "Stats: " << std::endl;
   std::sort(stats.begin(), stats.end(), std::greater<int>());
+  int choice;
   for (int vals : stats) {
     std::cout << vals << " " << std::flush;
     sleep(450);
+  }
+  for (int i = 0; i < 6; i++) {
+    std::cout << "What would you like to assign " << stats[i] << " to? "
+              << std::endl
+              << std::flush;
+    for (int j = 0; j < 6; j++) {
+      std::cout << j + 1 << ". " << statNames[j] << std::endl;
+    }
+    std::cin >> choice;
+    switch (choice) {
+    case 1:
+      Strength = stats[i];
+      statNames.erase(statNames.begin() + 0);
+      break;
+    case 2:
+      Dexterity = stats[i];
+      statNames.erase(statNames.begin() + 1);
+      break;
+    case 3:
+      Constitution = stats[i];
+      statNames.erase(statNames.begin() + 2);
+      break;
+    case 4:
+      Wisdom = stats[i];
+      statNames.erase(statNames.begin() + 3);
+      break;
+    case 5:
+      Intelligence = stats[i];
+      statNames.erase(statNames.begin() + 4);
+      break;
+    case 6:
+      Charisma = stats[i];
+      statNames.erase(statNames.begin() + 4);
+      break;
+    }
   }
 
   std::cout << std::endl;
